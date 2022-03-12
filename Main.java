a1b29b8159c17acbc879a722ed97e1eb45c94965
amit joshi <amitjoshi3101@gmail.com>
2022-03-12 12:11:44 +0530

    Changes done in JAVA file

--- Main.java
+++ Main.java
@@ -3,17 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-     Bkaccount b =new Bkaccount("Amit",12365);
      
-		b.deposit(1000);
-		b.withdraw(100);
-		b.withdraw(170);
-		b.withdraw(130);
-		
-		b.getaccNm(11);
-		
-		
-		System.out.println(b.tostring());
+   System.out.println("Hi , this file is updated from local repository");
 	}
 
 }
